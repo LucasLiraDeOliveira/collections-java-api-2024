e1f3e7460f846cfbc4e0f478e4d2384b56809947
LucasLiraDeOliveira <lucas.oliveira845@gmail.com>
2024-07-22 17:31:37 -0300

    Created a package to study Stream API and puted in a file to exercise Consumer interface

---
+++ src/main/streamAPI/ConsumerExample.java
@@ -0,0 +1,62 @@
+package main.streamAPI;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+/*
+ * Representa uma operação que aceita um argumento do tipo T e não retorna nenhum resultado.
+ * É utilizada principalmente para realizar ações, ou efeitos colaterais nos elementos do
+ * Stream sem modificar, ou retornar um valor.
+ */
+
+
+public class ConsumerExample {
+    public static void main(String[] args) {
+        // Criar uma lista de números inteiros
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+
+        // FORMA 1:
+        System.out.println("\nForma 1:");
+        // Usar o Consumer com expressão lambda para imprimir números pares
+        Consumer<Integer> imprimirNumeroPar = numero -> {
+            if (numero % 2 == 0) {
+                System.out.println(numero);
+            }
+        };
+
+        // Usar o Consumer para imprimir números pares no Stream
+        numeros.stream().forEach(imprimirNumeroPar);
+
+
+        // FORMA 2: caso não saiba implementar um Consumer
+        System.out.println("\nForma 2:");
+        numeros.stream().forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer n) {
+                if(n % 2 == 0){
+                    System.out.println(n);
+                }
+            }
+        });
+
+
+        // FORMA 3: usando lambda
+        System.out.println("\nForma 3:");
+        numeros.stream().forEach(n -> {
+            if(n % 2 == 0){
+                System.out.println(n);
+            }
+        });
+
+
+        // FORMA 4: simplificando mais ainda e só pondo o forEach
+        System.out.println("\nForma 4:");
+        numeros.forEach(n -> {
+            if(n % 2 == 0){
+                System.out.println(n);
+            }
+        });
+    }
+}
+
